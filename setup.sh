92daee89f2d43428cd03b2d25a0fd75558b49884
熊野朝仁 <kmntmht116@gmail.com>
2018-07-09 00:33:32 +0900

    up

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
- DOT_FILES=(.vimrc .tmux.conf .zshrc .zshenv)
+ DOT_FILES=(.vimrc .tmux .tmux.conf .zshrc .zshenv)
 
  for file in ${DOT_FILES[@]}
  do
      ln -s $HOME/dotfiles/$file $HOME/$file
  done
+
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+fi
+
+if [ ! -d ~/.zplug ]; then
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+fi
+
+
