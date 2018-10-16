eb1be3c118bf3f8c11825a17bdb906bd336a094d
kumamo-n <kmntmht116@gmail.com>
2018-10-16 14:29:25 +0900

    setup.sh start

--- setup.sh
+++ setup.sh
@@ -1,13 +1,27 @@
- DOT_FILES=(.vimrc .tmux .tmux.conf .zshrc .zshenv)
+ #!/bin/bash
 
- for file in ${DOT_FILES[@]}
- do
-     ln -s $HOME/dotfiles/$file $HOME/$file
- done
+set -e
 
-if [ ! -d ~/.tmux/plugins/tpm ]; then
-  git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+info() {
+    echo -e "\033[34m$@\033[m"
+}
+
+warn() {
+    echo -e "\033[33m$@\033[m"
+}
+
+error() {
+    echo -e "\033[31m$@\033[m"
+}
+
+DIR=$(pwd)
+
+
+if [[ ! -L $HOME/.dotfiles ]]; then
+    info "---> Link dotfiles into HOME directory"
+    ln -s $DIR $HOME/.dotfiles
 fi
+
 
 if [ ! -d ~/.zplug ]; then
   curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
